b43b027018af86b3f1d2732db86ecdc87b2adc20
Oxibug <agskitkat@gmail.com>
2019-09-06 10:15:41 +0300

    Lesson 4 ready

--- Lesson4/regulars.js
+++ Lesson4/regulars.js
@@ -1,3 +1,24 @@
+let str = `One: 'Hi Mary.' 
+Two: 'Oh, hi.'
+One: 'How are you doing?'
+Two: 'I'm doing alright. How about you?'
+One: 'Not too bad. The weather is great isn't it?'
+Two: 'Yes. It's absolutely beautiful today.'
+One: 'I wish it was like this more frequently.'
+Two: 'Me too.'
+One: 'So where are you going now?'
+Two: 'I'm going to meet a friend of mine at the department store'
+One: 'Going to do a little shopping?'
+Two: 'Yeah, I have to buy some presents for my parents.'
+One: 'What's the occasion?'
+Two: 'It's their anniversary.'
+One: 'That's great. Well, you better get going. You don't want to be late.'
+Two: 'I'll see you next time.'
+One: 'Sure.' Bye.'`;
+str = str.replace(/!?\B\'/gm, '"');
+console.log(str);
+
+
 class validForm {
     constructor(formId) {
         this.form = document.querySelector(formId);
@@ -6,17 +27,17 @@ class validForm {
             oneWord: {
                 regexp: /^\w+$/gm,
                 valid: 'Отлично',
-                novalid: 'Имя должно содержать только буквы'
+                invalid: 'Имя должно содержать только буквы'
             },
             email: {
                 regexp: /.*@.*\..{2,}/gm,
                 valid: 'Почта корректна',
-                novalid: 'Введите действительный адрес почты'
+                invalid: 'Введите действительный адрес почты'
             },
             phone: {
                 regexp: /\+\d\(\d{3}\)\d{3}\-\d{4}/gm,
                 valid: 'Телефон корректен',
-                novalid: 'Укажите телефон в формате: +7(000)000-0000'
+                invalid: 'Укажите телефон в формате: +7(000)000-0000'
             }
         };
 
@@ -36,16 +57,46 @@ class validForm {
                 this.inputHandler(field, input, feedback, pattern);
             });
         }
+
+        this.form.querySelector("#submit-form").addEventListener('click', event=> {
+            event.preventDefault();
+            if(this.allIsValid()) {
+                this.form.submit();
+            }
+        });
     }
 
     inputHandler(field, input, feedback, pattern) {
-        let result = input.value.match(this.patterns[pattern].regexp);
-        if(result) {
+        let cur_pattern = this.patterns[pattern];
+        let result = input.value.match(cur_pattern.regexp);
 
+        if(result) {
+            // Всё ОК !
+            field.isValid = true;
+            input.classList.remove(this.classNoValid);
+            input.classList.add(this.classVlaid);
+            feedback.innerText = cur_pattern.valid;
+            feedback.classList.add(this.classFeedbackVlaid);
+            feedback.classList.remove(this.classFeedbackNoVlaid);
+        } else {
+            // Сообщение о ошибке
+            field.isValid = false;
+            input.classList.add(this.classNoValid);
+            input.classList.remove(this.classVlaid);
+            feedback.innerText = cur_pattern.invalid;
+            feedback.classList.remove(this.classFeedbackVlaid);
+            feedback.classList.add(this.classFeedbackNoVlaid);
         }
     }
 
-
+    allIsValid() {
+        for(let field of this.fields) {
+            if(!field.isValid) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 new validForm('#form');
